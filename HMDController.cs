284efd0777cbfd8c0b56b455c413f0fb688e09c8
TOKIO <46534552+TOKIOatoz@users.noreply.github.com>
2020-12-10 00:57:30 +0900

    Add files via upload
    Add Code

---
+++ HMDController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HMDController : MonoBehaviour
+{
+    [SerializeField] GameObject baseHMD;
+    [SerializeField] float scaleX = 1;
+    [SerializeField] float scaleY = 1;
+    [SerializeField] float scaleZ = 1;
+    Vector3 targetPos = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform myTransform = this.transform;
+        targetPos.x = myTransform.localPosition.x * scaleX;
+        targetPos.y = myTransform.localPosition.y * scaleY;
+        targetPos.z = myTransform.localPosition.z * scaleZ;
+
+        myTransform.localPosition = targetPos;
+    }
+}
